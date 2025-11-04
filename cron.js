730571f5c7c134834a3bf551c04e47b4972a74e4
carleoj <jimroep@gmail.com>
2025-11-04 19:55:45 +0800

    refactor cron job setup to improve readability and structure

--- cron.js
+++ cron.js
@@ -1,14 +1,20 @@
 import cron from "cron";
 import https from "https";
 
-const job = new cron.CronJob("*/14 * * * *", function () {
+const wakeUp = () => {
   https
     .get(process.env.API_URL, (res) => {
       if (res.statusCode === 200) console.log("GET request sent successfully");
       else console.log("GET request failed", res.statusCode);
     })
     .on("error", (e) => console.error("Error while sending request", e));
-});
+};
+
+// Fire once immediately
+wakeUp();
+
+// Cron job every 14 minutes
+const job = new cron.CronJob("*/14 * * * *", wakeUp);
+job.start();
 
 export default job;
-
